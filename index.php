5af44c50e9bdce28ec1704a90e9197ffa88bb108
tareq <tareqabc@gmail.com>
2021-02-26 15:51:59 +0600

    asets bootstrap gitignore composer and index fils

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form design</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/fontawesome-free-5.13.0-web/css/all.min.css"/>
+    <link rel="stylesheet" href="Assets/css/bootstrap.css">
+    <link rel="stylesheet" href="Assets/fontawesome-free-5.13.0-web/css/all.min.css"/>
+    <link rel="stylesheet" href="Assets/css/style.css">
+    
+</head>
+<body>
+
+<h1>Add new students</h1>
+
+
+  <div class="form-group"></div>
+  <label for=""> Name</label>
+   <input class="form-control" type="text">
+   <div class="form-group"></div>
+   <label for=""> Email</label>
+    <input class="form-control" type="text">
+    <div class="form-group"></div>
+  <label for=""> Cell</label>
+   <input class="form-control" type="number">
+   <div class="form-group"></div>
+  <label for=""> Roll</label>
+   <input class="form-control" type="number">
+   <div class="form-group"></div>
+  <button class="btn btn-success">Insert</button>
+
+  <br> <br> <br>
+
+  
+  
+<script src="Assets/js/jquery-3.4.1.js"></script>
+
+<script src="Assets/js/proper.js"></script>
+<script src="Assets/js/bootstrap.js"></script>
+<script src="Assets/js/myscript.js"></script>
+
+
+
+</body>
+</html>
